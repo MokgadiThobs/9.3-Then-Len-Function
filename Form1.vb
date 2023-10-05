f6557ab657ecf8b5900629b40459ce571d17c1f1
Dell-User <Dell-User@DESKTOP-8EU8MHV>
2023-10-05 03:12:07 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub BtnShow_Click(sender As Object, e As EventArgs) Handles BtnShow.Click
+        Dim MyText As String
+        MyText = "Visual Basic 2022"
+        MsgBox(Len(MyText))
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
